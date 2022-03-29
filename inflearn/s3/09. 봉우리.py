f44080217d0c1201851a4141947a54d22e06cb4e
RegularKim <regular.jk.kim@gmail.com>
2022-03-29 14:51:33 +0900

    0329

--- inflearn/s3/09. 봉우리.py
+++ inflearn/s3/09. 봉우리.py
@@ -8,16 +8,8 @@ lst = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
 lst.insert(0, [0]*n)
 lst.append([0]*n)
 for i in lst:
-    i.insert(0, 0)
+    i.insert(0,0)
     i.append(0)
 
-cnt = 0
-x = [0, 1, 0, -1]
-y = [1, 0, -1, 0]
-
-for i in range(1, n+1):
-    for j in range(1, n+1):
-        if all(lst[i][j] > lst[i+x[k]][j+y[k]] for k in range(4)):
-            cnt += 1
-
-print(cnt)
+for i in lst:
+    print(*i)
--- inflearn/s4/02. 랜선자르기(결정알고리즘).py
+++ inflearn/s4/02. 랜선자르기(결정알고리즘).py
@@ -1,23 +1,16 @@
 import sys
 
 sys.stdin = open('02. 랜선자르기(결정알고리즘)', 'rt')
-n, m = map(int ,sys.stdin.readline().split())
-lst = [int(input()) for _ in range(n)]
-lst.sort()
-p1 = 1
-p2 = lst[-1]
-cnt = 0
+n, m = map(int, sys.stdin.readline().split())
+li = sorted([int(input()) for _ in range(n)])
+s = 1
+e = li[-1]
 
-while p1 <= p2:
-    mid = (p1 + p2) // 2
-    cnt = sum( line//mid for line in lst)
-    if cnt < m:
-        p2 = mid-1
-
+while s <= e:
+    mid = (s + e) // 2
+    cnt = sum(line // mid for line in li)
+    if cnt < m :
+        e = mid - 1
     else:
-        p1 = mid + 1
-
-
-print(cnt)
-
-# 아직 안 끝남!
+        s = mid + 1
+print(e) # 최대 개수 출력
